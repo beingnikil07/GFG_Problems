71b2780424aa90e234e725c23f9646bfcd4400d8
Nikil Kumar <115877620+beingnikil07@users.noreply.github.com>
2026-08-16 09:51:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count Odd and Even/count-odd-and-even.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] countOddEven(int[] arr) {
+        int odd=0;
+        int even=0;
+        for(int x:arr){
+            if(x%2==0){
+                even++;
+            }else{
+                odd++;
+            }
+        }
+        return new int[] {odd,even};
+    }
+}
