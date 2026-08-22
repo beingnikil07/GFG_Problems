4c301b054cae0d5dbfbacbe015a882b45bde5af3
Nikil Kumar <115877620+beingnikil07@users.noreply.github.com>
2026-08-22 11:21:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Find unique element/find-unique-element.java
@@ -0,0 +1,16 @@
+class Solution {
+	public int uniqueElement(int[] arr, int k) {
+		HashMap<Integer, Integer> map = new HashMap<>();
+		
+		// store all to HashMap
+		for (int x:arr) {
+			map.put(x, map.getOrDefault(x, 0) + 1);
+		}
+		for (int x:arr) {
+			if (map.get(x)<k) {
+				return x;
+			}
+		}
+		return -1;
+	}
+}
