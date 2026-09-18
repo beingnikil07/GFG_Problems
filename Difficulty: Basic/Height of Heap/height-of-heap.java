df7c0dca54434ff590a3d5486d7644ea5420ce61
Nikil Kumar <115877620+beingnikil07@users.noreply.github.com>
2026-09-18 14:52:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Height of Heap/height-of-heap.java
@@ -0,0 +1,12 @@
+class Solution {
+    static int heapHeight(int n, int arr[]) {   
+        if(n==1) return 1;
+        
+        int height=0;
+        while(n>1){
+            n=n/2;
+            height++;
+        }
+        return height;
+    }
+}
