3a7ef22a483de82607c736621026482117869384
Nikil Kumar <115877620+beingnikil07@users.noreply.github.com>
2026-09-23 20:23:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check Sorted Array/check-sorted-array.java
+++ Difficulty: Easy/Check Sorted Array/check-sorted-array.java
@@ -1,19 +1,11 @@
 class Solution {
 	public boolean isSorted(int[] arr) {
-	       
-	   if(arr.length==1){
-	       return true;
-	   }
-	
-	
 		for (int i = 1; i<arr.length; i++) {
-			if (arr[i]>=arr[i - 1]) {
-				
-			} else {
+			if (arr[i] >= arr[i - 1])
+				continue;
+			else
 				return false;
-			}
 		}
-		
 		return true;
 	}
 }
